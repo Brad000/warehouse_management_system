3a0a31861ce0b6f5406df5dc9e04d9c499e3172b
Bradley Best <bradleybest1989@gmail.com>
2020-04-10 22:51:14 +0000

    layout changed and established links with DB

--- app.py
+++ app.py
@@ -1,19 +1,27 @@
 import os
-import pymongo
-from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from flask import Flask, render_template, redirect, request, url_for, config
 from os import path
 if path.exists("env.py"):
     import env
 
-MONGODB_URI = os.environ.get("MONGO_URI")
-
 app = Flask(__name__)
+app.config['MONGO_URI'] = os.environ.get("MONGO_URI")
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
 @app.route('/manage_stock_cards')
 def manage_stock_cards():
-    return render_template("stockcard.html")
+    return render_template("managestockcard.html",
+                           stock_cards=mongo.db.stock_cards.find())
+
+
+@app.route('/edit_stock_cards')
+def edit_stock_cards():
+    return render_template('editstockcard.html')
 
 
 if __name__ == '__main__':
