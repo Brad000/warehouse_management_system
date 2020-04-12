5b2aed59f4cab2bcf602af0eadc5378098cce650
Bradley Best <bradleybest1989@gmail.com>
2020-04-12 10:40:03 +0000

    fixed bug on edit button, added edit functionality to editstockcard.html

--- app.py
+++ app.py
@@ -21,7 +21,8 @@ def manage_stock_cards():
 
 @app.route('/add_stock_cards')
 def add_stock_cards():
-    return render_template('addstockcard.html')
+    return render_template('addstockcard.html',
+                           customer=mongo.db.customer.find())
 
 
 @app.route('/insert_stock_cards', methods=['POST'])
@@ -31,9 +32,29 @@ def insert_stock_cards():
     return redirect(url_for('manage_stock_cards'))
 
 
-@app.route('/edit_stock_cards')
-def edit_stock_cards():
-    return render_template('editstockcard.html')
+@app.route('/edit_stock_cards/<stock_cards_id>')
+def edit_stock_cards(stock_cards_id):
+    stock_cards = mongo.db.stock_cards.find_one({"_id": ObjectId
+                                                (stock_cards_id)})
+    customer = mongo.db.customer.find()
+    return render_template('editstockcard.html',
+                           stock_cards=stock_cards, customer=customer)
+
+
+@app.route('/update_stock_cards/<stock_cards_id>', methods=["POST"])
+def update_stock_cards(stock_cards_id):
+    stock_cards = mongo.db.stock_cards
+    stock_cards.update({'_id': ObjectId(stock_cards_id)},
+                       {
+        'customer': request.form.get('customer'),
+        'product_code': request.form.get('product_code'),
+        'product_desc': request.form.get('product_desc'),
+        'unit_weight': request.form.get('unit_weight'),
+        'unit_per_pallet': request.form.get('unit_per_pallet'),
+        'suppler': request.form.get('supplier'),
+        'packaging': request.form.get('packaging'),
+    })
+    return redirect(url_for('manage_stock_cards'))
 
 
 if __name__ == '__main__':
