3b23d2b501d86bee3032fb392a8e458ae462f109
Bradley Best <bradleybest1989@gmail.com>
2020-04-30 19:33:53 +0000

    added files to requirements.txt

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import os
 import re
-from pymongo import PyMongo
-from bson.objectid import ObjectId
 from flask import Flask, render_template, redirect, request, flash, url_for, config
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 from os import path
 if path.exists("env.py"):
     import env
+
 
 app = Flask(__name__)
 app.config['MONGO_URI'] = os.environ.get("MONGO_URI")
