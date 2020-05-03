54c413aeae0a35da0b10cd06035ad7b33f02f2f5
Bradley Best <bradleybest1989@gmail.com>
2020-05-03 13:08:06 +0000

    login and register feature added

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import os
 import re
-from flask import Flask, render_template, redirect, request, flash, url_for
+from flask import Flask, render_template, redirect, request, flash, url_for, session
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from forms import LoginForm, RegisterForm
 from os import path
+import bcrypt
 if path.exists("env.py"):
     import env
 
@@ -19,6 +21,66 @@ mongo = PyMongo(app)
 @app.route('/home')
 def home():
     return render_template("index.html")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    """Login handler"""
+    if session.get('logged_in'):
+        if session['logged_in'] is True:
+            return redirect(url_for('home', title="Login"))
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        # get all users
+        users = mongo.db.users
+        # try and get one with same name as entered
+        db_user = users.find_one({'name': request.form['username']})
+
+        if db_user:
+            # check password using hashing
+            if bcrypt.hashpw(request.form['password'].encode('utf-8'),
+                             db_user['password']) == db_user['password']:
+                session['username'] = request.form['username']
+                session['logged_in'] = True
+                # successful redirect to home logged in
+                return redirect(url_for('home', title="Login", form=form))
+            # must have failed set flash message
+            flash('Invalid username/password combination')
+    return render_template("login.html", title="Login", form=form)
+
+
+@app.route('/logout')
+def logout():
+    """Clears session and redirects to home"""
+    session.clear()
+    return redirect(url_for('home'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    """Handles registration functionality"""
+    form = RegisterForm(request.form)
+    if form.validate_on_submit():
+        # get all the users
+        users = mongo.db.users
+        # see if we already have the entered username
+        existing_user = users.find_one({'name': request.form['username']})
+
+        if existing_user is None:
+            # hash the entered password
+            hash_pass = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
+            # insert the user to DB
+            users.insert_one({'name': request.form['username'],
+                          'password': hash_pass,
+                          'email': request.form['email']})
+            session['username'] = request.form['username']
+            return redirect(url_for('home'))
+        # duplicate username set flash message and reload page
+        flash('Sorry, that username is already taken - use another')
+        return redirect(url_for('register'))
+    return render_template('register.html', title='Register', form=form)
 
 
 @app.route('/goods_received')
